232541a87912f8e6f855613ab851fa4a79939272
tanzach <zachary.joseph.tan@gmail.com>
2024-04-27 03:21:36 +0800

    added authentication (buggy)

--- pages/2_Projects.py
+++ pages/2_Projects.py
@@ -3,46 +3,71 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-def animation_demo(df: pd.DataFrame) -> None:
-    # Display the DataFrame as a table
-    st.write(df)
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
 
 st.set_page_config(page_title="Projects", page_icon="📹")
-st.markdown("# Projects")
+
+names = ["John Smith", "Jose Rizal"]
+usernames = ["johnsmith", "jrizal"]
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "phildhrra_dashboard", "abcdef", cookie_expiry_days=0)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False: 
+    st.error("Username/Password is Incorrect")
+
+if authentication_status == None: 
+    st.warning("Please enter your username and password")
+
+if authentication_status: 
+
+    def animation_demo(df: pd.DataFrame) -> None:
+        # Display the DataFrame as a table
+        st.write(df)
+
+    
+    st.markdown("# Projects")
 
 
-csv_data = """
-Organization,Program Type,Programs/Projects Services,No. of Beneficiaries,Area Coverage (Pls. Specify Name of Areas),Area Coverage (Pls. Specify Name of Areas),Area Coverage (Pls. Specify Name of Areas)
-NGO 1,Sustainable Agricultural Productivity/Natural Resource Management,Sample Project,1200,Laguna,Something,Something
-NGO 2,Sustainable Agricultural Productivity/Natural Resource Management,Sample Project,1200,Pampanga,Something na,Something na
-NGO 2,Sustainable Agricultural Productivity/Natural Resource Management,Sample Project,1200,Laguna,Something pa,Something pa
-NGO 1,Basic Social Services Delivery,Sample Project,1200,Laguna,Something,Something
-NGO 1,Basic Social Services Delivery,Sample Project,1200,Pampanga,Something na,Something na
-NGO 2,Basic Social Services Delivery,Sample Project,1200,Laguna,Something,Something
-NGO 2,Basic Social Services Delivery,Sample Project,1200,Pampanga,Something na,Something na
-"""
+    csv_data = """
+    Organization,Program Type,Programs/Projects Services,No. of Beneficiaries,Area Coverage (Pls. Specify Name of Areas),Area Coverage (Pls. Specify Name of Areas),Area Coverage (Pls. Specify Name of Areas)
+    NGO 1,Sustainable Agricultural Productivity/Natural Resource Management,Sample Project,1200,Laguna,Something,Something
+    NGO 2,Sustainable Agricultural Productivity/Natural Resource Management,Sample Project,1200,Pampanga,Something na,Something na
+    NGO 2,Sustainable Agricultural Productivity/Natural Resource Management,Sample Project,1200,Laguna,Something pa,Something pa
+    NGO 1,Basic Social Services Delivery,Sample Project,1200,Laguna,Something,Something
+    NGO 1,Basic Social Services Delivery,Sample Project,1200,Pampanga,Something na,Something na
+    NGO 2,Basic Social Services Delivery,Sample Project,1200,Laguna,Something,Something
+    NGO 2,Basic Social Services Delivery,Sample Project,1200,Pampanga,Something na,Something na
+    """
 
-# Convert CSV data to DataFrame
-df = pd.read_csv(StringIO(csv_data), skiprows=1, header=0)
+    # Convert CSV data to DataFrame
+    df = pd.read_csv(StringIO(csv_data), skiprows=1, header=0)
 
-# Select options for organization and program type
-organizations = df['Organization'].unique()
-organizations = ['All'] + list(organizations)
-selected_ngo = st.selectbox('Select Organization: ', organizations)
+    # Select options for organization and program type
+    organizations = df['Organization'].unique()
+    organizations = ['All'] + list(organizations)
+    selected_ngo = st.selectbox('Select Organization: ', organizations)
 
-program_types = df['Program Type'].unique()
-program_types = ['All'] + list(program_types)
-selected_program_type = st.selectbox('Select Program Type: ', program_types)
+    program_types = df['Program Type'].unique()
+    program_types = ['All'] + list(program_types)
+    selected_program_type = st.selectbox('Select Program Type: ', program_types)
 
-# Filter DataFrame based on selected options
-if selected_ngo == 'All' and selected_program_type == 'All':
-    filtered_df = df
-elif selected_ngo == 'All':
-    filtered_df = df[df['Program Type'] == selected_program_type]
-elif selected_program_type == 'All':
-    filtered_df = df[df['Organization'] == selected_ngo]
-else:
-    filtered_df = df[(df['Organization'] == selected_ngo) & (df['Program Type'] == selected_program_type)]
+    # Filter DataFrame based on selected options
+    if selected_ngo == 'All' and selected_program_type == 'All':
+        filtered_df = df
+    elif selected_ngo == 'All':
+        filtered_df = df[df['Program Type'] == selected_program_type]
+    elif selected_program_type == 'All':
+        filtered_df = df[df['Organization'] == selected_ngo]
+    else:
+        filtered_df = df[(df['Organization'] == selected_ngo) & (df['Program Type'] == selected_program_type)]
 
-# Display the filtered DataFrame
-animation_demo(filtered_df)
+    # Display the filtered DataFrame
+    animation_demo(filtered_df)
--- pages/3_Finances.py
+++ pages/3_Finances.py
@@ -2,59 +2,83 @@ from typing import Any
 import streamlit as st
 import pandas as pd
 
-def finance_dashboard() -> None:
-    
-    st.write("### Average of Sources of Income or Revenues")
-    sources_data = {
-        'Source': ['Grant', 'Dues', 'Service Fees (Consultancy)', 'Donations',
-                   'Gains from Investments (interest earnings)', 'Endowment fund',
-                   'Social Enterprises', 'Others (Specify)'],
-        'Amount in PHP - Past Year': ['', '', '', '', '', '', '', ''],
-        'Amount in PHP - Current Year': ['', '', '', '', '', '', '', '']
-    }
-    sources_df = pd.DataFrame(sources_data)
-    st.table(sources_df)
-    
-    st.write("### Grants Data per Year")
-    grant_data = {
-        'Year': [2019, 2020, 2021],
-        'Grant Amount': [50000, 34000, 70000]  # Example data, replace with actual grant amounts
-    }
-    grant_df = pd.DataFrame(grant_data)
-    st.line_chart(grant_df.set_index('Year'))  # Line chart for grants per year
-    
-    st.write("### Finances per Organization")
-    # Selectbox to choose which NGO to display finance data
-    selected_ngo = st.selectbox('Select NGO', ['NGO 1', 'NGO 2'])
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
 
-    # Display the sources of income or revenues
-    st.write("Sources of Income or Revenues:")
-    sources_data = {
-        'Source': ['Grant', 'Dues', 'Service Fees (Consultancy)', 'Donations',
-                   'Gains from Investments (interest earnings)', 'Endowment fund',
-                   'Social Enterprises', 'Others (Specify)'],
-        'Amount in PHP - Past Year': ['', '', '', '', '', '', '', ''],
-        'Amount in PHP - Current Year': ['', '', '', '', '', '', '', '']
-    }
-    sources_df = pd.DataFrame(sources_data)
-    st.table(sources_df)
-
-    st.write("## Donor Organizations and Funding Partners")
-
-    # Display information about major sources of funds and grants
-    st.write("If major sources of funds are grants, indicate funding partner or donor organization and grant amount for the past 3 years.")
-    grant_data = {
-        'Donor/Funder': ['', '', ''],
-        'Project Funded': ['', '', ''],
-        'Year(s) Covered': ['', '', ''],
-        'Amount': ['', '', '']
-    }
-    grant_df = pd.DataFrame(grant_data)
-    st.table(grant_df)
-    
 # Set page title and icon
 st.set_page_config(page_title="Finance Dashboard", page_icon="💰")
-# Display the title
-st.markdown("# Finance Dashboard")
-# Display the finance dashboard
-finance_dashboard()
+
+names = ["John Smith", "Jose Rizal"]
+usernames = ["johnsmith", "jrizal"]
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False: 
+    st.error("Username/Password is Incorrect")
+
+if authentication_status == None: 
+    st.warning("Please enter your username and password")
+
+if authentication_status: 
+
+    def finance_dashboard() -> None:
+        
+        st.write("### Average of Sources of Income or Revenues")
+        sources_data = {
+            'Source': ['Grant', 'Dues', 'Service Fees (Consultancy)', 'Donations',
+                    'Gains from Investments (interest earnings)', 'Endowment fund',
+                    'Social Enterprises', 'Others (Specify)'],
+            'Amount in PHP - Past Year': ['', '', '', '', '', '', '', ''],
+            'Amount in PHP - Current Year': ['', '', '', '', '', '', '', '']
+        }
+        sources_df = pd.DataFrame(sources_data)
+        st.table(sources_df)
+        
+        st.write("### Grants Data per Year")
+        grant_data = {
+            'Year': [2019, 2020, 2021],
+            'Grant Amount': [50000, 34000, 70000]  # Example data, replace with actual grant amounts
+        }
+        grant_df = pd.DataFrame(grant_data)
+        st.line_chart(grant_df.set_index('Year'))  # Line chart for grants per year
+        
+        st.write("### Finances per Organization")
+        # Selectbox to choose which NGO to display finance data
+        selected_ngo = st.selectbox('Select NGO', ['NGO 1', 'NGO 2'])
+
+        # Display the sources of income or revenues
+        st.write("Sources of Income or Revenues:")
+        sources_data = {
+            'Source': ['Grant', 'Dues', 'Service Fees (Consultancy)', 'Donations',
+                    'Gains from Investments (interest earnings)', 'Endowment fund',
+                    'Social Enterprises', 'Others (Specify)'],
+            'Amount in PHP - Past Year': ['', '', '', '', '', '', '', ''],
+            'Amount in PHP - Current Year': ['', '', '', '', '', '', '', '']
+        }
+        sources_df = pd.DataFrame(sources_data)
+        st.table(sources_df)
+
+        st.write("## Donor Organizations and Funding Partners")
+
+        # Display information about major sources of funds and grants
+        st.write("If major sources of funds are grants, indicate funding partner or donor organization and grant amount for the past 3 years.")
+        grant_data = {
+            'Donor/Funder': ['', '', ''],
+            'Project Funded': ['', '', ''],
+            'Year(s) Covered': ['', '', ''],
+            'Amount': ['', '', '']
+        }
+        grant_df = pd.DataFrame(grant_data)
+        st.table(grant_df)
+        
+    # Display the title
+    st.markdown("# Finance Dashboard")
+    # Display the finance dashboard
+    finance_dashboard()
